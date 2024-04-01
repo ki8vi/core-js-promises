993ac7640291a55b54f7ed8e22c813a0bf73793d
ki8vi <oneofbln@gmail.com>
2024-04-01 11:48:52 +0600

    feat: solve getPromise task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,12 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const prom = new Promise((res, rej) => {
+    if (number < 0) rej(Error('promise that will be rejected'));
+    else res('promise that will be fulfilled');
+  });
+  return prom;
 }
 
 /**
