e2c4874140cc934d52662730e1ef0c39262df0fc
ki8vi <oneofbln@gmail.com>
2024-04-04 11:52:50 +0600

    feat: solve getAllResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -117,8 +117,11 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const successedPr = (pr) => {
+    return pr.then((res) => res).catch(() => null);
+  };
+  return Promise.all(promises.map(successedPr));
 }
 
 /**
